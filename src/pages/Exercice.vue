f56a7b01f3082ddef5157ea6d9bbfaeb8d5c4aca
Julien Rabin <jrabin@weborama.com>
2022-01-28 16:45:24 +0100

    add Pinia unstead vuex and use it for retrieve dishes

--- src/pages/Exercice.vue
+++ src/pages/Exercice.vue
@@ -12,51 +12,18 @@
   </q-page>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      dishes: [
-        {
-          id: 1,
-          image: "https://i.imgur.com/0umadnY.jpg",
-          nom: "Burger",
-          description:
-            "Un hamburger est un sandwich composé d'une ou plusieurs tranches de viande hachée, généralement du bœuf, placées dans un petit pain ou une brioche.",
-          note: 4
-        },
-        {
-          id: 2,
-          image: "https://i.imgur.com/b9zDbyb.jpg",
-          nom: "Pizza",
-          description:
-            "La pizza est un plat savoureux d'origine italienne, consistant en une base généralement ronde et aplatie de pâte levée à base de blé.",
-          note: 5
-        },
-        {
-          id: 3,
-          image: "https://i.imgur.com/RbKjUjB.jpg",
-          nom: "Petits choux",
-          description:
-            "Le chou de Bruxelles est une variété de chou, plante herbacée de la famille des Brassicaceae. C’est vraiement pas bon...",
-          note: 1
-        },
-        {
-          id: 4,
-          image: "https://i.imgur.com/xAuhNVg.jpg",
-          nom: "BBQ Ribs",
-          description:
-            "Les BBQ ribs ou barbecue ribs sont des grands classiques très appréciés partout dans le monde.",
-          note: 5
-        }
-      ],
-      showFormDishe: false
-    };
-  },
-  components: {
-    dishe: require("components/Dishe.vue").default,
-    "add-button": require("components/AddButton.vue").default,
-    "form-dishe": require("components/FormDishe.vue").default
-  }
-};
+<script setup> 
+import { ref } from 'vue'
+import { useStore } from 'src/stores/task.js'
+import { storeToRefs } from 'pinia'
+
+import AddButton from 'components/AddButton.vue'
+import Dishe from 'components/Dishe.vue'
+import FormDishe from 'components/FormDishe.vue'
+
+const store = useStore()
+
+const { dishes } = storeToRefs(store)
+const showFormDishe = ref(false)
+
 </script>
